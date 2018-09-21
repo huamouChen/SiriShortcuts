60da8995e3c30cc8d3c0b5c12224fed869fd063e
huamouChen <chenhm4444@gmail.com>
2018-09-21 15:14:42 +0800

    初始化项目

---
+++ SiriShortcuts/SiriShortcuts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcuts
+//
+//  Created by rex on 2018/9/21.
+//  Copyright © 2018 rex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
