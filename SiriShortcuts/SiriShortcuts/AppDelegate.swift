213e985a7a4a233819fd40643ad94de0511dc80a
huamouChen <chenhm4444@gmail.com>
2018-09-21 15:20:39 +0800

    siri shortcuts demo

--- SiriShortcuts/SiriShortcuts/AppDelegate.swift
+++ SiriShortcuts/SiriShortcuts/AppDelegate.swift
@@ -41,6 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        let viewController = window?.rootViewController as! ViewController
+        viewController.sayHi()
+        return true
+    }
 
 }
 
--- SiriShortcuts/SiriShortcuts/ViewController.swift
+++ SiriShortcuts/SiriShortcuts/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupIntents()
+    }
+    
+    func setupIntents() {
+        let activity = NSUserActivity(activityType: "info.huamouchen.SiriShortcuts.sayHi")
+        activity.title = "Say Hi"
+        activity.userInfo = ["speech" : "hi"]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = NSUserActivityPersistentIdentifier("info.huamouchen.SiriShortcuts.sayHi")
+        view.userActivity = activity
+        activity.becomeCurrent()
+    }
+    
+    public func sayHi() {
+        let alert = UIAlertController(title: "Hi There", message: "Hi There! Glad to see you got this working!", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 
 
